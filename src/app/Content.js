ffb411b4cb8e0717b8bd58295b481475571331cf
facesar <rostranespinoza@gmail.com>
2022-04-28 11:00:40 +0300

    files_projects

--- src/app/Content.js
+++ src/app/Content.js
@@ -23,7 +23,7 @@ const styles = {
         width: "100%",
         height: "30rem",
         borderRadius: "2rem",
-        padding: ["1rem", "0.5rem"],
+        padding: ["1rem", "1.5rem"],
         zIndex: 0,
         // marginTop: "10%",
     },
@@ -38,6 +38,7 @@ const styles = {
         border: "none",
         padding: "1rem",
         color: colors.bgText,
+        fontWeight: 800,
         "&:nth-child(1)": {
             color: colors.white,
             "&:hover": {
@@ -48,6 +49,33 @@ const styles = {
             color: colors.bgConnet,
             cursor: "pointer",
         }
+    },
+    card_header_icons: {
+        backgroundColor: "transparent",
+        border: "none",
+        padding: "1rem",
+        color: colors.bgText,
+        "&:hover": {
+            backgroundColor: colors.bgButton,
+            borderRadius: "1rem",
+            color: colors.white,
+            cursor: "pointer",
+        }
+    },
+    card_body_first: {
+        backgroundColor: colors.bgCard,
+        padding: "1.5rem",
+        // height: "20rem",
+        width: "100%",
+        borderRadius: "2rem",
+        marginTop: "2rem",
+        display: "flex",
+        justifyContent: "space-between",
+    },
+    card_body_first_content: {
+        display: "inline-flex",
+        flexDirection: "column",
+        margin: [0, 2]
     }
 }
 
@@ -64,9 +92,20 @@ const Content = () => {
                         <button class=${classes.card_header_btn}>P2P</button>
                     </div>
                     <div>
-                        <button class=${classes.card_header_btn}><i class="fa-solid fa-spinner"></i></button>
-                        <button class=${classes.card_header_btn}><i class="fa-solid fa-bars-progress"></i></button>
+                        <button class=${classes.card_header_icons}><i class="fa-solid fa-spinner"></i></button>
+                        <button class=${classes.card_header_icons}><i class="fa-solid fa-bars-progress"></i></button>
                     </div>
+                </div>
+                <div class=${classes.card_body_first}>
+                   <div class=${classes.card_body_first_content}>
+                        <span>You Sell</span>
+                        <button>Eth <i class="fa-solid fa-angle-down"></i> </button>
+                        <span>Etherum</span>
+                   </div>
+                   <div class=${classes.card_body_first_content}>
+                        <input></input>
+                        <span>75895858</span>
+                   </div>
                 </div>
             </div>
         </section>
