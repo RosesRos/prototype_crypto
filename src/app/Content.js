759acc47dc72aa2418d7bdfbcdb2158481fabf81
facesar <rostranespinoza@gmail.com>
2022-05-01 17:33:48 +0300

    files_projects

--- src/app/Content.js
+++ src/app/Content.js
@@ -16,11 +16,17 @@ const styles = {
         // display: "flex",
         // justifyContent: "center",
     },
-    card: {
+    section_inner: {
         position: "absolute",
         top: "20%",
         left: "50%",
         transform: "translate(-50%, 10%)",
+    },
+    card: {
+        // position: "absolute",
+        // top: "20%",
+        // left: "50%",
+        // transform: "translate(-50%, 10%)",
         backgroundColor: colors.bgCard2,
         maxWidth: "40rem",
         width: "100%",
@@ -65,6 +71,28 @@ const styles = {
             cursor: "pointer",
         }
     },
+    extra: {
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "1rem",
+        paddingTop: "1.5rem",
+    },
+    extra_span: {
+        fontSize: "1.4rem",
+
+    },
+    extra_btn: {
+        backgroundColor: "transparent",
+        color: colors.bgText,
+        border: "none",
+        padding: "0.5rem",
+        fontSize: "1.4rem",
+        "&:hover": {
+            color: colors.white,
+            cursor: "pointer",
+        }
+    }
     
 }
 
@@ -73,22 +101,33 @@ const {classes} = jss.createStyleSheet(styles).attach();
 const Content = () => {
     return `
         <section class=${classes.section}>
-            <div class=${classes.card}>
-                <div class=${classes.card_header}>
-                    <div>
-                        <button class=${classes.card_header_btn}>Swap</button>
-                        <button class=${classes.card_header_btn}>Limit</button>
-                        <button class=${classes.card_header_btn}>P2P</button>
+            <div class=${classes.section_inner}>
+                <div class=${classes.card}>
+                    <div class=${classes.card_header}>
+                        <div>
+                            <button class=${classes.card_header_btn}>Swap</button>
+                            <button class=${classes.card_header_btn}>Limit</button>
+                            <button class=${classes.card_header_btn}>P2P</button>
+                        </div>
+                        <div>
+                            <button class=${classes.card_header_icons}><i class="fa-solid fa-spinner"></i></button>
+                            <button class=${classes.card_header_icons}><i class="fa-solid fa-bars-progress"></i></button>
+                        </div>
                     </div>
-                    <div>
-                        <button class=${classes.card_header_icons}><i class="fa-solid fa-spinner"></i></button>
-                        <button class=${classes.card_header_icons}><i class="fa-solid fa-bars-progress"></i></button>
-                    </div>
+                    ${CardOne()}
+                    <span><i class="fa-solid fa-arrow-down"></i></span>
+                    ${CardTwo()}
+                    ${cardBtn()}
                 </div>
-                ${CardOne()}
-                <span><i class="fa-solid fa-arrow-down"></i></span>
-                ${CardTwo()}
-                ${cardBtn()}
+                <div class=${classes.extra}>
+                    <span class=${classes.extra_span}>Route</span>
+                    <button class=${classes.extra_btn}>
+                        ETH 
+                        <i class="fa-solid fa-angle-right"></i> 
+                        DAI
+                        <i class="fa-solid fa-arrow-trend-up"></i>
+                    </button>
+                </div>
             </div>
         </section>
     `
